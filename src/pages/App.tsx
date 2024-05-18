2aac6c0597fd00e2f4a0844ff3dc79c25f7c0c7e
Júlio Villa <julivicv@gmail.com>
2024-05-18 19:22:05 -0300

    Chore: load static donation items

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar"
+import Donation, { donation } from "../components/Donation"
 
 function App() {
 
@@ -13,11 +14,100 @@ function App() {
 		}
 	]
 
+	const donations:Array<donation> = [
+		{
+			id: '1',
+			icon: 'clothes',
+			name: 'Roupas',
+			description: 'roupas de inverno'
+		},
+		{
+			id: '2',
+			icon: 'pharma',
+			name: 'Medicamentos',
+			description: 'medicamentos gerais'
+		},
+		{
+			id: '3',
+			icon: 'blankets',
+			name: 'Cobertores',
+			description: ''
+		},{
+			id: '4',
+			icon: 'clothes',
+			name: 'Roupas',
+			description: 'roupas de inverno'
+		},
+		{
+			id: '5',
+			icon: 'pharma',
+			name: 'Medicamentos',
+			description: 'medicamentos gerais'
+		},
+		{
+			id: '6',
+			icon: 'blankets',
+			name: 'Cobertores',
+			description: ''
+		},{
+			id: '7',
+			icon: 'clothes',
+			name: 'Roupas',
+			description: 'roupas de inverno'
+		},
+		{
+			id: '8',
+			icon: 'pharma',
+			name: 'Medicamentos',
+			description: 'medicamentos gerais'
+		},
+		{
+			id: '9',
+			icon: 'blankets',
+			name: 'Cobertores',
+			description: ''
+		},{
+			id: '10',
+			icon: 'clothes',
+			name: 'Roupas',
+			description: 'roupas de inverno'
+		},
+		{
+			id: '11',
+			icon: 'pharma',
+			name: 'Medicamentos',
+			description: 'medicamentos gerais'
+		},
+		{
+			id: '12',
+			icon: 'blankets',
+			name: 'Cobertores',
+			description: ''
+		},{
+			id: '13',
+			icon: 'clothes',
+			name: 'Roupas',
+			description: 'roupas de inverno'
+		},
+		{
+			id: '14',
+			icon: 'pharma',
+			name: 'Medicamentos',
+			description: 'medicamentos gerais'
+		},
+		{
+			id: '15',
+			icon: 'blankets',
+			name: 'Cobertores',
+			description: ''
+		},
+	]
+
 	return (
 		<>
 			<Navbar menuActions={menuActions} />
 			<div>
-				Content
+				<Donation pix={'teste'} donations={donations}/>
 			</div>
 		</>
 	)
